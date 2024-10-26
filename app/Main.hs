d31e6a1b8ee99eba4a7242bc197069bb4325c06b
frazer <frazer@t480>
2024-10-26 16:33:12 +0100

    made cat cuter

--- app/Main.hs
+++ app/Main.hs
@@ -8,24 +8,33 @@ drawCat :: Diagram B
 drawCat = mconcat
   [
     -- Eyes
-  circle 0.1 # fc black # translate (r2 (-0.4, 0.3)),
-  circle 0.1 # fc black # translate (r2 (0.4, 0.3)),
+  circle 0.13 # fc black # translate (r2 (-0.4, 0.25)),
+  circle 0.13 # fc black # translate (r2 (0.4, 0.25)),
+
   -- Nose
-  triangle 0.1 # fc pink # rotateBy (1/2) # translate (r2 (0, 0)),
+  triangle 0.26 # fc pink # rotateBy (1/2) # translate (r2 (0, 0)),
+
+  -- Mouth
+  vrule 0.4 # lw veryThin # translate (r2 (0, -0.2)),
+  hrule 0.4 # lw veryThin # translate (r2 (0, -0.4)),
 
     -- Whiskers
-  hrule 0.6 # lw veryThin # translate (r2 (-0.5, 0)),
-  hrule 0.6 # lw veryThin # translate (r2 (0.5, 0)),
-  hrule 0.6 # lw veryThin # translate (r2 (-0.5, -0.1)),
-  hrule 0.6 # lw veryThin # translate (r2 (0.5, -0.1)),
-  hrule 0.6 # lw veryThin # translate (r2 (-0.5, 0.1)),
-  hrule 0.6 # lw veryThin # translate (r2 (0.5, 0.1)),
+  hrule 0.6 # lw veryThin # translate (r2 (-1, 0)),
+  hrule 0.6 # lw veryThin # translate (r2 (1, 0)),
+  hrule 0.6 # lw veryThin # translate (r2 (-1, -0.1)),
+  hrule 0.6 # lw veryThin # translate (r2 (1, -0.1)),
+  hrule 0.6 # lw veryThin # translate (r2 (-1, -0.2)),
+  hrule 0.6 # lw veryThin # translate (r2 (1, -0.2)),
 
   -- Head
-  circle 1 # fc lightgray # lw none,
-    -- Ears
-  triangle 0.5 # scaleX 0.6 # fc lightgray # translate (r2 (-0.7, 0.8)),
-  triangle 0.5 # scaleX 0.6 # fc lightgray # translate (r2 (0.7, 0.8))
+  circle 1  # fc lightgray
+            # lw veryThick
+            # lc pink
+            # dashingG [0.2, 0.05] 0,
+
+  -- Ears
+  triangle 0.5 # scaleX 0.6 # fc lightgray # translate (r2 (-0.6, 0.8)),
+  triangle 0.5 # scaleX 0.6 # fc lightgray # translate (r2 (0.6, 0.8))
   ]
 
 -- Main function to render the cat to "cat.svg"
